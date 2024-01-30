96d924c0c631af690411b7a6a37394faf7c4e215
Shreyas Deodhare <67466611+shreyasdeodhare@users.noreply.github.com>
2024-01-30 13:01:52 +0530

    Update constants.py

--- com/project/common/constants.py
+++ com/project/common/constants.py
@@ -1,6 +1,6 @@
 DB_USER = "root"
 DB_PASSWORD = "shreyas"
-DB_HOST = "172.17.255.255"
+DB_HOST = "172.17.0.1"
 DB_NAME = "ecommerce"
 
 QUERY_INSERT_Customer  = "INSERT INTO Customer (cid, cname, c_email, c_number) VALUES (%s, %s, %s,%s)"
