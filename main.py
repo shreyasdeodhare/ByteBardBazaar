c62f0514309dc851f189f64053b29da3bc37d8e1
Shreyas Deodhare <67466611+shreyasdeodhare@users.noreply.github.com>
2024-01-31 08:38:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from  com.project.controller.CwppController.CwppController import app
 
 
 if __name__ == '__main__':
-    app.run(host="127.0.0.1", port=8000, debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=True)
